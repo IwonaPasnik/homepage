0ef935f97debf711dec65ba89e4f0ce2e5f64050
Iwona Pasnik <Iwona.Pasnik@gmail.com>
2022-08-01 19:48:05 +0200

    added semicolons in js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 {
     const welcome = () => {
         console.log("Cześć!");
-    }
+    };
 
     const toggleBackground = () => {
         const body = document.querySelector(".js-body");
         const themeName = document.querySelector(".js-buttonChange");
         body.classList.toggle("bodychange");
         themeName.innerText = body.classList.contains("bodychange") ? "jasny" : "ciemny";
-    }
+    };
 
     const init = () => {
         const button = document.querySelector(".js-button");
         button.addEventListener("click", toggleBackground);
         welcome();
-    }
+    };
 
     init();
 }
